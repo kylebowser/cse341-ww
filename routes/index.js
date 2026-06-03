aab8f3bc6963a62a7860eedcbcd1215f53d022da
kylebowser <kylebowser9@gmail.com>
2026-06-02 22:35:31 -0600

    auth update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
-const routes = require('express').Router();
-const baseControl = require('../controllers/baseControl');
-const validation = require('../middleware/validate');
+const routes = require("express").Router();
+const baseControl = require("../controllers/baseControl");
+const validation = require("../middleware/validate");
 
-routes.get('/', baseControl.getData);
+routes.get("/", baseControl.getData);
 
-routes.get('/contacts', baseControl.getAll);
+routes.get("/contacts", baseControl.getAll);
 
-routes.get('/contacts/:id', baseControl.getSingle);
+routes.get("/contacts/:id", baseControl.getSingle);
 
-routes.post('/contacts', validation.saveContact, baseControl.createContact);
+routes.post("/contacts", validation.saveContact, baseControl.createContact);
 
-routes.put('/contacts/:id', validation.saveContact, baseControl.updateContact);
+routes.put("/contacts/:id", validation.saveContact, baseControl.updateContact);
 
-routes.delete('/contacts/:id', baseControl.deleteContact);
+routes.delete("/contacts/:id", baseControl.deleteContact);
+
+routes.get("/login", passport.authenticate("github"), (req, res) => {});
+
+routes.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,27 +1,96 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./dataBase/connect');
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("./dataBase/connect");
 //const professionalRoutes = require('./routes/professional');
-const base = require('./routes/index');
-const swaggerFile = require('./swagger_output.json');
-const swaggerUi = require('swagger-ui-express');
-
+const base = require("./routes/index");
+const swaggerFile = require("./swagger_output.json");
+const swaggerUi = require("swagger-ui-express");
+const passport = require("passport");
+const session = require("express-session");
+const GithubStrategy = require("passport-github").Strategy;
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app
   .use(bodyParser.json())
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+    }),
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key Authorization",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    );
     next();
   })
-  .use('/', base);
+  .use(cors({ methods: ["GET", "POST", "PUT", "UPDATE", "PATCH", "DELETE"] }))
+  .use(cors({ origin: "*" }))
+  .use("/", require("./routes/index.js"));
 
-process.on('uncaughtException', (err, origin) => {
-    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-  });
+process.on("uncaughtException", (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`,
+  );
+});
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(null, profile);
+      // });
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged out",
+  );
+});
+
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  },
+);
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
@@ -32,4 +101,4 @@ mongodb.initDb((err, mongodb) => {
   }
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
