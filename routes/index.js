9fac1f01bcb38c7092975e129de2b682937f1a8c
kylebowser <kylebowser9@gmail.com>
2026-06-02 22:35:46 -0600

    index

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,29 @@
 const routes = require("express").Router();
 const baseControl = require("../controllers/baseControl");
 const validation = require("../middleware/validate");
+const auth = require("../middleware/authenticate.js");
 
 routes.get("/", baseControl.getData);
 
 routes.get("/contacts", baseControl.getAll);
 
-routes.get("/contacts/:id", baseControl.getSingle);
+routes.get("/contacts/:id", auth, baseControl.getSingle);
 
-routes.post("/contacts", validation.saveContact, baseControl.createContact);
+routes.post(
+  "/contacts",
+  auth,
+  validation.saveContact,
+  baseControl.createContact,
+);
 
-routes.put("/contacts/:id", validation.saveContact, baseControl.updateContact);
+routes.put(
+  "/contacts/:id",
+  auth,
+  validation.saveContact,
+  baseControl.updateContact,
+);
 
-routes.delete("/contacts/:id", baseControl.deleteContact);
+routes.delete("/contacts/:id", auth, baseControl.deleteContact);
 
 routes.get("/login", passport.authenticate("github"), (req, res) => {});
 
